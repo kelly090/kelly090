dcf274d4d588bcc1551227b347303f4fa5004310
Kelly F <kelly.forster@hotmail.co.uk>
2022-05-02 18:02:13 +0100

    Tutorial

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,5 +1,6 @@
 import random
 
+
 def guess(x):
     random_number = random.randint(1, x)
     guess = 0
@@ -10,5 +11,6 @@ def guess(x):
         elif guess > random_number:
             print("Sorry, guess again. Too high.")
     print(f"Yay, congrats! You have guessed the number {random_number} correctly.")
+
 
 guess(10)
